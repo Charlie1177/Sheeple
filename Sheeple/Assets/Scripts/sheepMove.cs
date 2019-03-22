82a78ad859bec3923874c3a479b0763a3f901e48
psiDaylight <46943944+psiDaylight@users.noreply.github.com>
2019-03-21 19:36:59 -0500

    Spawning&Movement

---
+++ Sheeple/Assets/Scripts/sheepMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Notes: Speed Variable changes movement speed of Object.
+// Stopping Distance variable changes distance in which moving object stops from target.
+
+public class sheepMove : MonoBehaviour
+{
+
+    public float speed;
+    public float stoppingDistance;
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
