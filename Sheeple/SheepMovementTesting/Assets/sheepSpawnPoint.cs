b610f6419f8ebc8de21372e84690eb28f263cde6
psiDaylight <46943944+psiDaylight@users.noreply.github.com>
2019-03-21 19:18:42 -0500

    Movement&SpawnTest

---
+++ Sheeple/SheepMovementTesting/Assets/sheepSpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheepSpawnPoint : MonoBehaviour
+{
+
+    public float spawnTime = 10f;
+    public float spawnDelay = 10f;
+
+    public GameObject[] Enemy;
+
+    public Vector3 enposition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Update", spawnDelay, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int enemyIndex = Random.Range(0, Enemy.Length);
+        Instantiate(Enemy[enemyIndex], enposition, transform.rotation);
+    }
+}
