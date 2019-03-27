8bb19bd2d0ddadd6eb8593427b076d7f7e2099d2
psiDaylight <46943944+psiDaylight@users.noreply.github.com>
2019-03-26 20:33:14 -0500

    sheepMoveCode

---
+++ Sheeple/Assets/Scripts/sheepSpawner2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheepSpawner2 : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject enemy;
+    public float spawnTime = 5f;
+    public float spawnDelay = 3f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("addEnemy", spawnDelay, spawnTime);
+    }
+
+    void addEnemy()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
