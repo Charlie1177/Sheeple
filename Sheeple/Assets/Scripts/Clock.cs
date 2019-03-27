4b15f8a3c218c4bbeb544b754f960de602a95eb4
dtconcha <42611457+dtconcha@users.noreply.github.com>
2019-03-26 20:25:18 -0500

    updated timer, fully functional

--- Sheeple/Assets/Scripts/Clock.cs
+++ Sheeple/Assets/Scripts/Clock.cs
@@ -10,14 +10,17 @@ public class Clock : MonoBehaviour
 {
     public int DateFetcher;
     public int hours;
-    public Text Hoursul, Minutestul, Secondsul;
+    public Text Hoursul, Minutestul, Secondsul, Cycle;
     public float tics;
+    public string apm;
     // Start is called before the first frame update
     void Start()
     {
         tics = 0;
         hours = 5;
+        apm = "AM";
         Hoursul.text = hours.ToString();
+        Cycle.text = apm.ToString();
     }
 
     // Update is called once per frame
@@ -26,12 +29,28 @@ public class Clock : MonoBehaviour
 
         tics = tics + Time.deltaTime;
        
-        if (tics >= 10)
+        if (tics >= 2)
         {
             hours++;
             Hoursul.text = hours.ToString();
             tics = 0;
         }
+
+        if(hours >= 12)
+        {
+            hours = 0;
+            if(apm == "AM")
+            {
+                apm = "PM";
+                Cycle.text = apm.ToString();
+
+            }else if(apm == "PM"){
+
+                apm = "AM";
+                Cycle.text = apm.ToString();
+            }
+        }
+
            //This if or assigning times
         //Hoursul.text = DateFetcher.Hour.ToString();
         //Minutestul.text = DateFetcher.Minute.ToString();
