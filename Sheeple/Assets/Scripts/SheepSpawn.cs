865ecdf0623557707099c3051183a5f937f16674
dtconcha <42611457+dtconcha@users.noreply.github.com>
2019-03-28 19:51:22 -0500

    Implimented sheep spawn

---
+++ Sheeple/Assets/Scripts/SheepSpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepSpawn : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject sheep;
+    public float spawnTime = 5f;
+    public float spawnDelay = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("addSheep", spawnDelay, spawnTime);
+    }
+
+    void addSheep()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(sheep, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
