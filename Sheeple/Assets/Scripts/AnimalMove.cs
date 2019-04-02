0c2d8cf5a75a21bf2c62be181265d160dad52aaf
dvminder <46944023+dvminder@users.noreply.github.com>
2019-04-02 18:59:39 -0500

    QA and changes to movetest

--- Sheeple/Assets/Scripts/AnimalMove.cs
+++ Sheeple/Assets/Scripts/AnimalMove.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class AnimalMove : MonoBehaviour
 {
-
+    public float health = 100;
+    private float realHealth = 100;
+    [Header("Unity Stuff")]
+    public Image healthbar;
+    
     public float speed = 1.5f;
     private Vector3 target;
     private int moved = 0;
@@ -22,7 +27,19 @@ public class AnimalMove : MonoBehaviour
                 target.z = transform.position.z;
             }
             transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            
         }
         
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "pain")
+            Debug.Log("Entered");
+        float damage = 15;
+        health = realHealth;
+        health -= damage;
+        realHealth = health;
+        health = health / 100f;
+        healthbar.fillAmount = health;
     }
 }
---
+++ Sheeple/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+   
+    [HideInInspector]
+    public float health = 100;
+    
+
+    [Header("Unity Stuff")]
+    public Image healthbar;
+
+    void Start()
+    {
+        
+    }
+
+    /*void OnCollisionEnter2D(Collision2D collision)
+    { 
+        float damage = 0;
+        health -= damage;
+        health = health / 100f;
+        healthbar.fillAmount = health;
+    }*/
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "pain")
+        Debug.Log("Entered"); 
+        float damage = 15;
+        health -= damage;
+        health = health / 100f;
+        healthbar.fillAmount = health;
+    }
+}
