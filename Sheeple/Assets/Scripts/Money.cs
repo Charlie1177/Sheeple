fee16370ce68f94f97767256f676d67d4854ca95
dtconcha <42611457+dtconcha@users.noreply.github.com>
2019-04-04 14:31:18 -0500

    updates

---
+++ Sheeple/Assets/Scripts/Money.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class Money : MonoBehaviour
+{
+   
+    public int money;
+    public Text moneyCount;
+    public float tics;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tics = 0;
+        money = 100;
+        moneyCount.text = money.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        tics = tics + Time.deltaTime;
+       
+        if (tics >= 1)
+        {
+            money++;
+            moneyCount.text = money.ToString();
+            tics = 0;
+        }
+
+        /*if(hours >= 12)
+        {
+            hours = 0;
+            if(apm == "AM")
+            {
+                apm = "PM";
+                Cycle.text = apm.ToString();
+
+            }else if(apm == "PM"){
+
+                apm = "AM";
+                Cycle.text = apm.ToString();
+            }
+        }*/
+
+           //This if or assigning times
+        //Hoursul.text = DateFetcher.Hour.ToString();
+        //Minutestul.text = DateFetcher.Minute.ToString();
+        //Secondsul.text = DateFetcher.Second.ToString();
+        //print("print:" + DateFetcher);
+    }
+}
--- Sheeple/Assets/Scripts/spawnAnimal.cs
+++ Sheeple/Assets/Scripts/spawnAnimal.cs
@@ -17,6 +17,8 @@ public class spawnAnimal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
